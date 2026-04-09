8aa1c1d0eeb9ce35dd7557c2d6e284b5fc4a83df
unknown <tolaganov03@list.ru>
2026-04-09 18:34:10 +0500

    new change

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   // Add this line here:
-  base: "/web_utilities/", 
+  base: "/", 
   
   server: {
     host: "::",
